32b48ca826da9182f58ab4afc0e4967674fb4686
Poonam-Sama <guptapoonam1994@gmail.com>
2020-11-25 15:09:33 +0530

    new file

---
+++ 012updated/prog2/prog2.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	x := 42 //short declaration operator can both declare and assign
+	fmt.Println(x)
+	y := "hey guys i am adding repo to git"
+
+}
