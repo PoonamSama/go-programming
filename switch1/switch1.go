d50f665ac6d2b76927ff79923be9ef2c3dd7dbb7
Poonam-Sama <guptapoonam1994@gmail.com>
2020-11-27 22:11:22 +0530

    switch using fallthrough

---
+++ switch1/switch1.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	switch {
+	case false:
+		fmt.Println("This should not print")
+	case (2 == 9):
+		fmt.Println("This should not print either")
+	case (3 == 3):
+		fmt.Println("This should  print")
+		fallthrough
+	case (1 == 4):
+		fmt.Println("not true, does it print?")
+		fallthrough
+	case (3 < 1):
+		fmt.Println("This should not  print")
+
+	}
+}
