910198a56c21e00fbe46463d382d7525a69870e3
Poonam-Sama <guptapoonam1994@gmail.com>
2020-11-28 23:40:06 +0530

    intro to map

---
+++ Map1/Map1.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	m := map[string]int{ //composite literal map[string]int: this is type   and in {} are the "key" :values
+		"JB":  32,
+		"MMP": 27,
+		"KG":  12,
+	}
+	fmt.Println(m)
+}
