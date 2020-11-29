0d4e75fcc5c50945c1077394fb2368f12b4b1f30
Poonam-Sama <guptapoonam1994@gmail.com>
2020-11-29 22:21:06 +0530

    intro to struct

---
+++ struct1/struct1.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+type people struct {
+	firstname string
+	lastname  string
+	age       int
+}
+
+func main() {
+	p1 := people{
+		firstname: "James",
+		lastname:  "Bond",
+		age:       33,
+	}
+	p2 := people{
+		firstname: "Poonam",
+		lastname:  "Gupta",
+	}
+	p3 := people{}
+	fmt.Println(p1)
+	fmt.Println(p2)
+	fmt.Println(p1.age)
+	fmt.Println(p2.firstname)
+	fmt.Println(p3)
+}
