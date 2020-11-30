9a8562e36d374ccc18c5ddbeaffc7953914386bc
Poonam-Sama <guptapoonam1994@gmail.com>
2020-11-30 22:43:16 +0530

    Assertion using switch in interface

---
+++ interfaceAssertion/Assertion.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+)
+
+type person struct { //keyword identifier typeofvariable
+	first string
+	last  string
+}
+
+// var x int : keyword identifier type(int,struct,interface,bool etc)
+type secretAgent struct {
+	person
+	ltk bool
+}
+
+// func (r receiver) identifier(parameters) (return(s)) { code }
+
+func (s secretAgent) speak() { //speak is the METHOD; secretAgent has METHOD speak()
+	fmt.Println("I am", s.first, s.last, "from secretAgent speak")
+}
+func (s person) speak() { //person also has has METHOD speak()
+	fmt.Println("I am", s.first, s.last, "from person speak")
+}
+
+type human interface { //interface says:if you have this METHOD,then you are my TYPE
+	speak()
+}
+
+func bar(h human) {
+	fmt.Println("I was passed into func bar", h)
+	switch h.(type) {
+	case secretAgent:
+		fmt.Println("I was passed in barrrr", h.(secretAgent).last) //h.(secretAgent is assertion, not conversion like in type hotdog int)
+	case person:
+		fmt.Println("I was passed in barrrr", h.(person).last)
+	}
+}
+func main() {
+	sa1 := secretAgent{ //the value in{} is of type secretAgent;and a value can be of more than one type;sa1 is variable
+		person: person{
+			"James",
+			"Bond",
+		},
+		ltk: true,
+	}
+
+	sa2 := secretAgent{
+		person: person{
+			"Miss",
+			"Moneypenny",
+		},
+		ltk: true,
+	}
+	p1 := person{
+		"Dr.",
+		"Yes",
+	}
+
+	fmt.Println(sa1)
+	//sa1.speak()
+	//sa2.speak()
+	//p1.speak()
+	bar(sa1) //the fn bar can take type secretAgent and type person : this is called polymorphism
+	bar(sa2) //because the fn bar takes type human
+	bar(p1)  // and type human interface takes speak(); and speak (r receives) both secretAgent and person
+}
