665b273ed3a9f79b0ea0256d2418411fd5d0c0cf
Poonam-Sama <guptapoonam1994@gmail.com>
2020-12-08 07:14:50 +0530

    concurrency means many things happening at once, and we are using sync primitive to coordinate or synchronise them

---
+++ concurWAITGROUP/WAITGROUP.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	fmt.Println("OS   \t", runtime.GOOS) //constant string, dont need parentheses  to call
+	fmt.Println("GOARCH \t", runtime.GOARCH)
+	fmt.Println("CPUs  \t", runtime.NumCPU())         //these are func(),take no parameter,return int
+	fmt.Println("GoRoutines", runtime.NumGoroutine()) //NumCPU and NumGoroutine, dont add s
+	wg.Add(1)
+	go foo()
+	bar()
+	fmt.Println("CPUs  \t", runtime.NumCPU())
+	fmt.Println("GoRoutines", runtime.NumGoroutine())
+	wg.Wait()
+
+}
+func foo() {
+	for i := 0; i < 6; i++ {
+		fmt.Println("foo:", i)
+	}
+	wg.Done()
+}
+
+func bar() {
+	for i := 0; i < 6; i++ {
+		fmt.Println("bar:", i)
+	}
+}
