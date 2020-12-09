fae47d8233f8a3c401cc49cc36fd9fce480fb224
Poonam-Sama <guptapoonam1994@gmail.com>
2020-12-09 23:36:37 +0530

    cannot use c1 (type chan<- int) as type <-chan int in assignment > >  cannot use c2 (type <-chan int) as type chan int in assignment > but we can assign type chan int to type <-chan int or chan<- int,although it doesnt change their type

--- Channel/ChannelBlock/008dosntAssign/DoesntAssign.go
+++ Channel/ChannelBlock/008dosntAssign/DoesntAssign.go
@@ -10,9 +10,15 @@ func main() {
 	fmt.Printf("type of c1\t%T\n", c1)
 	fmt.Printf("type of c2\t%T\n", c2)
 
-	c2 = c //doesnt give err, but also doesnt change type
-	fmt.Printf("type of c2\t%T\n", c2)
-	c = c2  //err: cannot use c2 (type <-chan int) as type chan int in assignment
-	c2 = c1 // also gives error: cannot use c1 (type chan<- int) as type <-chan int in assignment
+	//	c2 = c //doesnt give err, but also doesnt change type
+	//fmt.Printf("type of c2\t%T\n", c2)
+	//c = c2  //err: cannot use c2 (type <-chan int) as type chan int in assignment
+	//c2 = c1 // also gives error: cannot use c1 (type chan<- int) as type <-chan int in assignment
+	//general to specific can  convert
+	fmt.Printf("c: %T\n", (chan<- int)(c))
+	//specific to general cant convert
+	//fmt.Printf("c2: %T\n", (chan int)(c2)) //gives error
+	//sp to sp cant convert
+	fmt.Printf("c1: %T\n", (<-chan int)(c1)) //err cant convert
 
 }
