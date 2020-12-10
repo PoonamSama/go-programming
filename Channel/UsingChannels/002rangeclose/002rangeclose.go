973d988c8cd670c6c729ec88585ca0c7cbef941f
Poonam-Sama <guptapoonam1994@gmail.com>
2020-12-10 15:34:25 +0530

    Ranging over a channel and close(channnel_name) closing is necessary to avoid deadlock error

--- Channel/UsingChannels/002rangeclose/002rangeclose.go
+++ Channel/UsingChannels/002rangeclose/002rangeclose.go
@@ -8,11 +8,11 @@ func main() {
 		for i := 0; i < 50; i++ {
 			c <- i
 		}
-		close(c) //if you dnt close the channel,it will keep waiting for more values and
+		close(c) //if you dnt close the channel,it will keep waiting for more values BECZ OF (RANGE C) and
 	}() //fatat error:all goroutines asleep,deadlock
 
 	for v := range c {
-		fmt.Println(v) //to range over a channel
+		fmt.Println(v) //to range over a channel; CLOSING CHANNEL IS NECESSARY
 	}
 	fmt.Println("about to exit code")
 }
