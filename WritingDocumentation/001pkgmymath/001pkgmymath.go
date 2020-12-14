748e67edaefdd4d7e8506779b061c83388b71cb5
Poonam-Sama <guptapoonam1994@gmail.com>
2020-12-14 15:04:07 +0530

    using package mymath from godoc.org that we created and uploaded , in this code

--- WritingDocumentation/001pkgmymath/001pkgmymath.go
+++ WritingDocumentation/001pkgmymath/001pkgmymath.go
@@ -1,10 +1,10 @@
 //you can't have blank lines
 //otherwise they wont be printed
 
-//Package mymath provides ACME math solutions
+//Package mymath provides ACME math solutions :must start with Package <pkgname>
 package mymath
 
-//Sum adds a number of integers and returns an int
+//Sum adds a number of integers and returns an int:must start with a capital letter function name;cap means can be exported
 func Sum(xi ...int) int {
 	sum := 0
 	for _, v := range xi {
---
+++ WritingDocumentation/001pkgmymath/usingpkgmymath/usingpkgmymath.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	mymath "github.com/GoesToEleven/go-programming/WritingDocumentation/001pkgmymath"
+)
+
+func main() {
+	fmt.Println("3+4 is ", mymath.Sum(3, 4))
+}
