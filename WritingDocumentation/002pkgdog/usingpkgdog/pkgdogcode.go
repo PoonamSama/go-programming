a3247ff9b2324c1c51ea8427f04d07e1046e716c
Poonam-Sama <guptapoonam1994@gmail.com>
2020-12-14 16:13:18 +0530

    1 human yr is 7 dogyears

---
+++ WritingDocumentation/002pkgdog/usingpkgdog/pkgdogcode.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	dog "github.com/GoesToEleven/go-programming/WriteInterface/002pkgdogyears"
+)
+
+func main() {
+	hy := 3
+	fmt.Println("dog yrs for 3 humanyears is:", dog.Dogyears(hy))
+}
