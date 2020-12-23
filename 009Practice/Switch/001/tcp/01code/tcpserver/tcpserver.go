868d882ddc28f941aa24e8ed5383b16326116308
Poonam-Sama <guptapoonam1994@gmail.com>
2020-12-23 15:48:01 +0530

    tcp server takes input then gives time to the client

---
+++ 009Practice/Switch/001/tcp/01code/tcpserver/tcpserver.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"time"
+)
+
+func main() {
+	ln, err := net.Listen("tcp", ":8080")
+	ercheck(err)
+	defer ln.Close()
+	connx, err := ln.Accept()
+	ercheck(err)
+	for {
+		netData, err := bufio.NewReader(connx).ReadString('\n')
+		ercheck(err)
+		fmt.Println(string(netData))
+		t := time.Now()
+		myTime := t.Format(time.RFC3339) + "\n"
+		connx.Write([]byte(myTime))
+
+	}
+
+}
+func ercheck(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
