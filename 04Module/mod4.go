32f61eb84add96305854ea08ca1b6fcfe81c480c
Poonam-Sama <guptapoonam1994@gmail.com>
2021-01-06 13:05:44 +0530

    REST API using Gin

--- 04Module/mod4.go
+++ 04Module/mod4.go
@@ -7,74 +7,68 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var balance = 9563
-var transactionCount = 0
-
-//INPUT exported
-type INPUT struct {
-	AMOUNT int `json:"amount"`
+type withdrawValue struct {
+	AMOUNT int `json:"amount" `
 }
 
 func main() {
+	balance := 9563
+	transactionCount := 0
 	server := gin.Default()
 	server.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{"Your balance is": balance})
+		c.JSON(200, gin.H{"Your balance is": getBalance(&balance)})
 	})
 	server.POST("/", func(c *gin.Context) {
-		var input INPUT
-		c.BindJSON(&input)
-		c.JSON(200, gin.H{"Result": transaction(input.AMOUNT)})
+		var clientInput withdrawValue
+		c.BindJSON(&clientInput)
+		c.JSON(200, gin.H{"Result": withdrawAmount(clientInput.AMOUNT, &balance, &transactionCount)}) // Your custom response here
 	})
 	server.POST("/exit", func(c *gin.Context) {
-		c.JSON(200, "Exit")
 		os.Exit(1)
 	})
 	server.Run(":8080") // listen for incoming connections
 }
-func transaction(clientInput int) string { //response func for method POST/
-	if transactionCount >= 5 {
+func getBalance(balanceptr *int) string {
+	message := fmt.Sprintf("%d", *balanceptr)
+	return message
+}
+func withdrawAmount(clientVal int, balancePtr *int, counterPtr *int) string {
+	if *counterPtr >= 5 {
 		fmt.Println("Error! you have made maximum number of transactions for today: 5.")
-		message := fmt.Sprintf("Maximum transactions reached for a day:5.You can exit by POST/exit.")
+		message := fmt.Sprintf("Maximum transactions reached for a day:5. You can exit using POST/exit.")
 		return message
 	}
-	if balance < 100 {
-		fmt.Println("Balance less than 100. You can't withdraw.")
-		message := fmt.Sprintf("Balance less than 100.You can't withdraw.You can exit by POST/exit")
+	if *balancePtr < 100 {
+		message := fmt.Sprintf("Balance less than 100.You can't withdraw anymore.You can exit using POST/exit.")
 		return message
 	}
-	checkValue, amount := checkInput(clientInput, balance)
+	checkValue, amount := isAmountValid(clientVal, *balancePtr)
 	switch checkValue {
 	case 0:
-		fmt.Println("Error!Please enter ONLY ONE natural number")
 		message := ("Error!Please enter ONE natural number")
 		return message
 	case 10:
-		fmt.Println("Error!Please enter a Positive value")
 		message := fmt.Sprintf("Error!Please enter a Positive value ")
 		return message
 	case 20:
-		fmt.Println("Error!Please enter amount divisible by 100 ")
 		message := fmt.Sprintf("Error!Please enter amount divisible by 100 ")
 		return message
 	case 30:
-		fmt.Println("Error! Please enter amount less than or equal to 5000")
 		message := fmt.Sprintf("Error! Please enter amount less than or equal to 5000 ")
 		return message
 	case 40:
-		fmt.Printf("Error!Please enter amount less than or equal to your account balance that is: %v\n", balance)
 		message := fmt.Sprintf("Error!Please enter amount less than or equal to your account balance")
 		return message
 	default:
-		transactionCount++
-		i := denominations(amount)
-		balance = balance - amount
-		fmt.Println("\n TRANSACTION SUCCESSFUL.YOUR BALANCE NOW IS\n:", balance)
-		j := fmt.Sprintf("TRANSACTION SUCCESSFUL. ")
+		*counterPtr++
+		i := getDenominations(amount)
+		*balancePtr = *balancePtr - amount
+		j := fmt.Sprintf("TRANSACTION SUCCESSFUL.")
 		message := i + j
 		return message
 	}
 }
-func checkInput(input int, balance int) (int, int) { //func to check valid amount given by client
+func isAmountValid(input int, balance int) (int, int) {
 	if input == 0 {
 		return 0, input
 	}
@@ -93,7 +87,7 @@ func checkInput(input int, balance int) (int, int) { //func to check valid amoun
 	return 50, input
 }
 
-func denominations(total int) string { //func to find out denominations of currency
+func getDenominations(total int) string {
 	var q1 int
 	var q2 int
 	var q3 int
