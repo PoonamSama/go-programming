0d6e2c1a5e474b8ed7a874e48b6dbcda79e7463c
bheem singh chouhan <84610737+bheemsa011@users.noreply.github.com>
2021-09-09 08:11:06 +0530

    const can't declare by :=

---
+++ BheemSingh/010_const_declaration/declare_constant.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	const a = 11
+	const b = 20
+
+	const (
+		c = 25
+		d = 33
+		e = 55
+	)
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+
+}
