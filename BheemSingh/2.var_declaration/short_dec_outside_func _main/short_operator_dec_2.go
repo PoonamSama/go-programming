38ad86e00e6cb7d45881110524ade810151b4312
bheem singh chouhan <84610737+bheemsa011@users.noreply.github.com>
2021-09-10 13:01:02 +0530

    this program won't work

---
+++ BheemSingh/2.var_declaration/short_dec_outside_func _main/short_operator_dec_2.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// short declaration operator won't work outside func main body
+ x:=12
+
+ // if we use as mention below this program will work
+// var x = 12
+
+func main() {
+
+	fmt.Println(x)
+
+}
